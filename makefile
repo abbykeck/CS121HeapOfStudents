f61541258ad488529591c7dcdd92f16f36b5a878
abbykeck <abigailkeck2425@gmail.com>
2025-10-03 20:25:25 +0000

    finished student class

--- makefile
+++ makefile
@@ -1,12 +1,15 @@
 CFLAGS = -g -Wall
-heapStudents: address.o date.o main.o
-	g++ $(CFLAGS) address.o date.o main.o -o heapStudents
+heapStudents: address.o date.o student.o main.o
+	g++ $(CFLAGS) address.o date.o student.o main.o -o heapStudents
 
 address.o: address.cpp address.h
 	g++ $(CFLAGS) -c address.cpp
 
 date.o: date.cpp date.h
 	g++ $(CFLAGS) -c date.cpp
+
+student.o: student.cpp student.h address.h date.h
+	g++ $(CFLAGS) -c student.cpp
 
 main.o: main.cpp address.h
 	g++ $(CFLAGS) -c main.cpp
@@ -20,3 +23,6 @@ clean:
 
 debug: heapStudents
 	gdb heapStudents
+
+valgrind: heapStudents
+	valgrind --leak-check=yes ./heapStudents
---
+++ student.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <sstream>
+#include "address.h"
+#include "date.h"
+#include "student.h"
+
+Student::Student() {
+	Student::studentAddress = new Address();
+	Student::birthDate = new Date();
+	Student::gradDate = new Date();
+	Student::firstName = "";
+	Student::lastName = "";
+	Student::creditHours = 0;
+} // end constructor
+
+Student::~Student() {
+	delete Student::studentAddress;
+	delete Student::birthDate;
+	delete Student::gradDate;
+} // end deconstructor
+
+void Student::init(std::string studentString) {
+	std::stringstream ss;
+	ss.clear();
+	ss.str(studentString);
+	getline(ss, Student::firstName, ',');
+	getline(ss, Student::lastName, ',');
+	std::string street;
+	std::string city;
+	std::string state;
+	std::string zip;
+	getline(ss, street, ',');
+	getline(ss, city, ',');
+	getline(ss, state, ',');
+	getline(ss, zip, ',');
+	Student::studentAddress->init(street, city, state, zip);
+	std::string bDate;
+	std::string gDate;
+	getline(ss, bDate, ',');
+	getline(ss, gDate, ',');
+	Student::birthDate->init(bDate);
+	Student::gradDate->init(gDate);
+	std::string hours;
+	getline(ss, hours);
+	ss.clear();
+	ss.str("");
+	ss << hours;
+	ss >> Student::creditHours;
+} // end init
+
+void Student::printStudent() {
+	std::cout << Student::firstName << " " << Student::lastName << std::endl;
+	Student::studentAddress->printAddress();
+	std::cout << "DOB: ";
+	Student::birthDate->printDate();
+	std::cout << "Grad: ";
+	Student::gradDate->printDate();
+	std::cout << "Credits: " << Student::creditHours << std::endl;
+	for (int i = 0; i < 36; i++) {
+		std::cout << "_";
+	} // end for
+	std::cout << std::endl;
+} // end printStudent
+
+std::string Student::getLastFirst() {
+	std::stringstream ss;
+	ss << Student::lastName << ", " << Student::firstName;
+	return ss.str();
+} // end getLastFirst
